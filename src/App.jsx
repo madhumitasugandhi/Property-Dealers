8e4e701273ebb89ecde9fe0afc3927bf472d5f8c
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-07-30 12:13:15 +0530

    HomeCard heading added and Footer Copyright part added.

--- src/App.jsx
+++ src/App.jsx
@@ -94,13 +94,84 @@ const App = () => {
     <Router>
       <Navbar />
       {showModal && <ContactModal onClose={() => setShowModal(false)} />}
-      
+
       <Routes>
         <Route
           path="/"
           element={
             <>
               <Hero />
+              <div style={{ textAlign: 'center', margin: '3rem 0' }}>
+  <h2 style={{
+    fontSize: '2.5rem',
+    fontWeight: '800',
+    color: '#1f2937',
+    position: 'relative',
+    display: 'inline-block'
+  }}>
+    <span style={{ position: 'relative', display: 'inline-block', marginRight: '6px' }}>
+      
+      {/* Large Box */}
+      <span style={{
+        position: 'absolute',
+        top: '-15px',
+        left: '-10px',
+        width: '60px',
+        height: '60px',
+        backgroundColor: '#b80000',
+        borderRadius: '6px',
+        zIndex: -2,
+        
+      }}></span>
+
+      {/* Small Box */}
+      <span style={{
+        position: 'absolute',
+        top: '-30px',
+        left: '28px',
+        width: '30px',
+        height: '30px',
+        backgroundColor: '#b80000',
+        border:'1px solid white',
+        borderRadius: '6px',
+        zIndex: -1,
+        
+      }}></span>
+
+      Builder
+    </span>
+     Floor For Sale
+  </h2>
+
+  <p style={{
+  fontSize: '1.125rem',
+  color: '#323233ff',
+  maxWidth: '600px',
+  margin: '0.5rem auto 0',
+  textAlign: 'center',
+}}>
+  Find Your <span style={{
+    position: 'relative',
+    display: 'inline-block',
+    color: '#323233ff'
+  }}>
+    Perfect Home with a Trusted Real
+    <span style={{
+      position: 'absolute',
+      left: 0,
+      bottom: -16,
+      width: '100%',
+      height: '3px',
+      backgroundColor: '#b80000',
+      borderRadius: '2px',
+    }}></span>
+  </span> Estate Agent
+</p>
+
+
+</div>
+
+
               <div className="card-grid">
                 {properties.map((property) => (
                   <HomeCard key={property.id} {...property} />
@@ -114,7 +185,7 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
       </Routes>
 
-      
+
       <Footer />
     </Router>
   );
