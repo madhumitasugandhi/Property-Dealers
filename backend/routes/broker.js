a424b9ce8aa32bbbef46aae244a0bbcf4d6c7db9
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-08-12 17:02:42 +0530

    updated

--- backend/routes/broker.js
+++ backend/routes/broker.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBroker, getBrokers } from '../controllers/Broker.js';
+import { createBroker, getBrokers, updateBroker } from '../controllers/Broker.js';
 
 const router = express.Router();
 
 router.post('/', createBroker);
 router.get('/', getBrokers);
+router.put('/:id', updateBroker); // for updating broker status
 
 export default router;
