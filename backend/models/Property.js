0950346f37dbb7fd4728124834f5694ef585221e
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-08-16 14:27:23 +0530

    updated fields

--- backend/models/Property.js
+++ backend/models/Property.js
@@ -1,31 +1,45 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
 
-const Property = sequelize.define('Properties', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  title: { type: DataTypes.STRING, allowNull: false },
-  location: { type: DataTypes.STRING, allowNull: false },
-  price: { type: DataTypes.DECIMAL(12, 2), allowNull: false },
-  width: { type: DataTypes.FLOAT },
-  length: { type: DataTypes.FLOAT },
-  area: { type: DataTypes.FLOAT },
-  bhk: { type: DataTypes.STRING },
-  floor: { type: DataTypes.STRING },
-  type: { type: DataTypes.ENUM('flat', 'farm', 'shop', 'land'), allowNull: false },
-  image_path: { type: DataTypes.STRING },
-  created_at: { type: DataTypes.DATE, allowNull: false },
-  broker_id: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: 'Broker',
-      key: 'id'
-    }
+const Property = sequelize.define(
+  "Properties",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    location: { type: DataTypes.STRING, allowNull: false },
+    totalPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    width: { type: DataTypes.FLOAT },
+    length: { type: DataTypes.FLOAT },
+    area: { type: DataTypes.FLOAT },
+    bhk: { type: DataTypes.STRING },
+    floor: { type: DataTypes.STRING },
+    propertyType: {
+      type: DataTypes.ENUM("Flat", "Farm", "Shop", "Land"),
+      allowNull: true,
+    },
+    images: {
+      type: DataTypes.JSON,
+      allowNull: true,
+      defaultValue: [],
+    },
+    created_at: { type: DataTypes.DATE, allowNull: false },
+    broker_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "Broker",
+        key: "id",
+      },
+    },
+    taluka: { type: DataTypes.TEXT },
+    description: { type: DataTypes.TEXT },
   },
-  taluka: { type: DataTypes.TEXT },
-  description: { type: DataTypes.TEXT }
-}, {
-  timestamps: false
-});
+  {
+    timestamps: false,
+  }
+);
 
 export default Property;
